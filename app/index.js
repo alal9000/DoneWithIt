cc0d0535b4f59f7ecb96140118b8fb19c50b9638
Aaron Lal <lucidlear@gmail.com>
2024-08-29 10:28:30 +1000

    input components with dynamic width

--- app/index.js
+++ app/index.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import MessagesScreen from './screens/MessagesScreen';
+import ListingEditScreen from './screens/ListingEditScreen';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
-        <MessagesScreen />
+        <ListingEditScreen />
         <StatusBar style="dark" />
       </SafeAreaView>
     </SafeAreaProvider>
