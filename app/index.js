8b717884db3f5dbd338f5d22ce43171a27a8ccec
Aaron Lal <lucidlear@gmail.com>
2024-08-30 09:58:18 +1000

    accessing the library

--- app/index.js
+++ app/index.js
@@ -1,10 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
+import { Image } from 'react-native';
+
 import Screen from './components/Screen';
+import Button from './components/Button';
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
   const requestPermission = async () => {
     const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (!granted) alert('You need to enable permission to access the library');
@@ -14,10 +19,25 @@ export default function App() {
     requestPermission();
   }, []);
 
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled) setImageUri(result.assets[0].uri);
+    } catch (error) {
+      console.log('error reading an image', error);
+    }
+  };
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
-        <Screen />
+        <Screen>
+          <Button title="Select Image" onPress={selectImage} />
+          <Image
+            source={{ uri: imageUri }}
+            style={{ width: 200, height: 200 }}
+          />
+        </Screen>
         <StatusBar style="dark" />
       </SafeAreaView>
     </SafeAreaProvider>
