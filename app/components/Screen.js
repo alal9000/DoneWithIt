feb3d08d1df066b3c9a7523dabb2118951997ccf
Aaron Lal <lucidlear@gmail.com>
2024-08-29 10:06:41 +1000

    improving the messages screen

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-import colors from '../config/colors';
 
 function Screen({ children }) {
   return <View style={styles.screen}>{children}</View>;
